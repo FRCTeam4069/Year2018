cc79df9e50369d611338b973b97a6ce00fdb5ec1
Redrield <redrield@gmail.com>
2018-02-14 20:56:38 -0500

    Fixed up PID implementation

--- src/main/java/frc/team4069/robot/util/PIDdle.java
+++ src/main/java/frc/team4069/robot/util/PIDdle.java
@@ -1,229 +1,214 @@
 package frc.team4069.robot.util;
 
-public class PIDdle
-{
-  public static final int PIDDLE_MANUAL = 0;
-  public static final int PIDDLE_AUTOMATIC = 1;
-  public static final int PIDDLE_DIRECT = 0;
-  public static final int PIDDLE_REVERSE = 1;
+public class PIDdle {
 
-  private int controllerDirection = PIDDLE_DIRECT;
+    public static final int PIDDLE_MANUAL = 0;
+    public static final int PIDDLE_AUTOMATIC = 1;
+    public static final int PIDDLE_DIRECT = 0;
+    public static final int PIDDLE_REVERSE = 1;
 
-  /* working variables */
-  private long mLastTimeCalled;
+    private int controllerDirection = PIDDLE_DIRECT;
 
-  private double mCurrentInput;
- 
-  private double mPIDOutput;
+    /* working variables */
+    private long mLastTimeCalled;
 
-  private double mDesiredOutput;
-  
-  private double mIntegralAccumulator;
-  private double mLastInputValue;
-  
-  private double mKP, mKI, mKD;
-  
-  private long mMinimumTimeBetweenComputeCalls = 1000; // 1 sec if called < this value, will return and wait till at least
-                                       //this amount of time has passed before computing next value
-  
-  private double mOutputMinimumValue = Double.MIN_VALUE;
-  
-  private double mOutputMaximumValue=Double.MAX_VALUE;
-  
-  boolean mAutomaticMode = false;
+    private double mCurrentInput;
 
-  
-  /**
-   * Compute : Called as frequently as possible with current input value to PID
-   * 
-   * 
-   * @param CurrentInput - input value to PID
-   * @return PID output value
-   */
-  
-  public double Compute(double CurrentInput)
-  {
-    if (!mAutomaticMode)
-      return mPIDOutput;
-    
-    mCurrentInput = CurrentInput;  //remember for when re-enabling after disable
-    
-    long now = System.currentTimeMillis();
+    private double mPIDOutput;
 
-    long deltaTime = (now - mLastTimeCalled);
+    private double mDesiredOutput;
 
-    if (deltaTime >= mMinimumTimeBetweenComputeCalls)
-    {
-      double error = mDesiredOutput - CurrentInput; // Get error between current Input and wanted Setpoint
+    private double mIntegralAccumulator;
+    private double mLastInputValue;
+    private double lastError;
 
-      mIntegralAccumulator += (mKI * error); // Accumulate the integral term
+    private double mKP, mKI, mKD;
 
-      if (mIntegralAccumulator > mOutputMaximumValue)
-        mIntegralAccumulator = mOutputMaximumValue;
-      else
-        if (mIntegralAccumulator < mOutputMinimumValue)
-          mIntegralAccumulator = mOutputMinimumValue;
+    private long mMinimumTimeBetweenComputeCalls = 1000; // 1 sec if called < this value, will return and wait till at least
+    //this amount of time has passed before computing next value
 
-      double derivativeInput = (CurrentInput - mLastInputValue); // compute derivative input from last call
+    private double mOutputMinimumValue = Double.MIN_VALUE;
 
-      
-      mPIDOutput = mKP * error + mIntegralAccumulator - mKD * derivativeInput;
+    private double mOutputMaximumValue = Double.MAX_VALUE;
 
-      // Now clamp the output
-      if (mPIDOutput > mOutputMaximumValue)
-        mPIDOutput = mOutputMaximumValue;
-      else
-        if (mPIDOutput < mOutputMinimumValue)
-          mPIDOutput = mOutputMinimumValue;
-
-      /* Remember some variables for next time */
-
-      mLastInputValue = CurrentInput;
-      mLastTimeCalled = now;
-    } // if enough time has passed
-    
-    return mPIDOutput;
-  } // Compute
+    private boolean mAutomaticMode = false;
 
 
-  /**
-   * GetCurrentOutput 
-   * @return current PID output value
-   */
-  double GetCurrentOutput()
-  {
-    return mPIDOutput;
-  }
-  
-  
-  
-  /**
-   * SetTunings : Kp,Ki,kD parameters, NOTE NEVER send negative values for these.  If you want the PID
-   * to run with negative outputs, call the setDirection with PIDDLE_REVERSE
-   * 
-   * @param Kp  : proportional constant
-   * @param Ki  : integral constant
-   * @param Kd  : Derivative constant
-   */
-  
-  
-  public void SetTunings(double Kp, double Ki, double Kd)
-  {
-    if (Kp < 0 || Ki < 0 || Kd < 0)
-      return;
+    /**
+     * Compute : Called as frequently as possible with current input value to PID
+     *
+     * @param currentInput - input value to PID
+     * @return PID output value
+     */
+    public double compute(double currentInput) {
+        if (!mAutomaticMode) {
+            return mPIDOutput;
+        }
 
-    double SampleTimeInSec = ((double) mMinimumTimeBetweenComputeCalls) / 1000;
-    mKP = Kp;
-    mKI = Ki * SampleTimeInSec;
-    mKD = Kd / SampleTimeInSec;
+        mCurrentInput = currentInput;  //remember for when re-enabling after disable
 
-    // if pid values will be negative, adjust params to be negative
-    if (controllerDirection == PIDDLE_REVERSE)
-    {
-      mKP = (0 - mKP);
-      mKI = (0 - mKI);
-      mKD = (0 - mKD);
+        long now = System.currentTimeMillis();
+
+        long deltaTime = (now - mLastTimeCalled);
+
+        if (deltaTime >= mMinimumTimeBetweenComputeCalls) {
+            double error = mDesiredOutput
+                    - currentInput; // Get error between current Input and wanted Setpoint
+
+            mIntegralAccumulator += (mKI * error); // Accumulate the integral term
+
+            if (mIntegralAccumulator > mOutputMaximumValue) {
+                mIntegralAccumulator = mOutputMaximumValue;
+            } else if (mIntegralAccumulator < mOutputMinimumValue) {
+                mIntegralAccumulator = mOutputMinimumValue;
+            }
+
+            double derivativeError = lastError - error; // Compute the derivative of the error
+
+            mPIDOutput = mKP * error + mIntegralAccumulator + mKD * derivativeError;
+
+            // Now clamp the output
+            if (mPIDOutput > mOutputMaximumValue) {
+                mPIDOutput = mOutputMaximumValue;
+            } else if (mPIDOutput < mOutputMinimumValue) {
+                mPIDOutput = mOutputMinimumValue;
+            }
+
+            /* Remember some variables for next time */
+
+            lastError = error;
+            mLastInputValue = currentInput;
+            mLastTimeCalled = now;
+        } // if enough time has passed
+
+        return mPIDOutput;
     }
-  } // SetTunings
 
-  
-  /**
-   * SetMinimumSampleTime : New minimum sample time in milliseconds
-   * if compute() is called faster than this, it will do nothing until at LEAST this amount of
-   * time has passed.
-   * @param NewSampleTime : new minimum sample time in milliseconds
-   */
-  public void SetMinimumSampleTime(long NewSampleTime)
-  {
-    if (NewSampleTime > 0)
-    {
-      double ratio = (double) NewSampleTime / (double) mMinimumTimeBetweenComputeCalls;
 
-      mKI *= ratio;  //scale I&D so we don't get a 'kick' when sample time changes
-      mKD /= ratio;
-
-      mMinimumTimeBetweenComputeCalls = NewSampleTime;
+    /**
+     * getCurrentOutput
+     *
+     * @return current PID output value
+     */
+    double getCurrentOutput() {
+        return mPIDOutput;
     }
-  } // SetSampleTime
 
 
-  /**
-   * SetOutputLimits : It is STRONGLY encouraged that you set these to reasonable values.
-   * 
-   * @param Min : minimum output value allowed/wanted by PID control
-   * @param Max : maximum output value allowed/wanted by PID control
-   */
-  public void SetOutputLimits(double Min, double Max)
-  {
-    if (Min > Max)
-      return;
+    /**
+     * getTunings : Kp,Ki,kD parameters, NOTE NEVER send negative values for these.  If you want the PID
+     * to run with negative outputs, call the setDirection with PIDDLE_REVERSE
+     *
+     * @param kP : proportional constant
+     * @param kI : integral constant
+     * @param kD : Derivative constant
+     */
+    public void setTunings(double kP, double kI, double kD) {
+        if (kP < 0 || kI < 0 || kD < 0) {
+            return;
+        }
 
-    mOutputMinimumValue = Min;
-    mOutputMaximumValue = Max;
+        double SampleTimeInSec = ((double) mMinimumTimeBetweenComputeCalls) / 1000;
+        mKP = kP;
+        mKI = kI * SampleTimeInSec;
+        mKD = kD / SampleTimeInSec;
 
-    if (mPIDOutput > mOutputMaximumValue)
-      mPIDOutput = mOutputMaximumValue;
-    else
-      if (mPIDOutput < mOutputMinimumValue)
-        mPIDOutput = mOutputMinimumValue;
-    
-    if (mIntegralAccumulator > mOutputMaximumValue)
-      mIntegralAccumulator = mOutputMaximumValue;
-    else
-      if (mIntegralAccumulator < mOutputMinimumValue)
-        mIntegralAccumulator = mOutputMinimumValue;
-  }// SetOutputLimits
+        // if pid values will be negative, adjust params to be negative
+        if (controllerDirection == PIDDLE_REVERSE) {
+            mKP = (0 - mKP);
+            mKI = (0 - mKI);
+            mKD = (0 - mKD);
+        }
+    } // setTunings
 
-  
-  /**
-   * SetMode : Set the PID controller to PIDDLE_AUTOMATIC or PIDDLE_MANUAL
-   *  in Manual mode compute() will do nothing.
-   *  
-   * @param Mode  PIDDLE_AUTOMATIC or PIDDLE_MANUAL
-   */
-  
-  public void SetMode(int Mode)
-  {
-    boolean newAuto = (Mode == PIDDLE_AUTOMATIC);
 
-    if (newAuto == !mAutomaticMode)
-    { 
-      Initialize();
+    /**
+     * SetMinimumSampleTime : New minimum sample time in milliseconds
+     * if compute() is called faster than this, it will do nothing until at LEAST this amount of
+     * time has passed.
+     *
+     * @param newSampleTime : new minimum sample time in milliseconds
+     */
+    public void setMinimumSampleTime(long newSampleTime) {
+        if (newSampleTime > 0) {
+            double ratio = (double) newSampleTime / (double) mMinimumTimeBetweenComputeCalls;
+
+            mKI *= ratio;  //scale I&D so we don't get a 'kick' when sample time changes
+            mKD /= ratio;
+
+            mMinimumTimeBetweenComputeCalls = newSampleTime;
+        }
     }
-    mAutomaticMode = newAuto;
-  } // SetMode
 
-  
-  
-  /**
-   * Initialize : This is called by SetMode when Automatic is enabled to resume PID operations without
-   * hopefully any 'jumps' or 'kicks'.  It is not intended to be called from outside the class
-   *  (thus the private designation)
-   */
-  
-  private void Initialize()
-  {
-    mLastInputValue = mCurrentInput;
-    mIntegralAccumulator = mPIDOutput;
-    
-    if (mIntegralAccumulator > mOutputMaximumValue)
-      mIntegralAccumulator = mOutputMaximumValue;
-    else
-      if (mIntegralAccumulator < mOutputMinimumValue)
-        mIntegralAccumulator = mOutputMinimumValue;
-  } // Initialize
 
-  
-  /**
-   * SetControllerDirection : If the PID output should be positive PIDDLE_DIRECT should be set
-   * if the PID output is meant to be negative, PIDDLE_REVERSE should be set.
-   * This parameter auto-sets the tuning parameters to negative, NEVER send negative tuning values to
-   * kI,kP,kD
-   * @param Direction
-   */
-  
-  public void SetControllerDirection(int Direction)
-  {
-    controllerDirection = Direction;
-  } // SetControllerDirection
-} // class PIDdle
+    /**
+     * SetOutputLimits : It is STRONGLY encouraged that you set these to reasonable values.
+     *
+     * @param min minimum output value allowed/wanted by PID control
+     * @param max maximum output value allowed/wanted by PID control
+     */
+    public void setOutputLimits(double min, double max) {
+        if (min > max) {
+            return;
+        }
+
+        mOutputMinimumValue = min;
+        mOutputMaximumValue = max;
+
+        if (mPIDOutput > mOutputMaximumValue) {
+            mPIDOutput = mOutputMaximumValue;
+        } else if (mPIDOutput < mOutputMinimumValue) {
+            mPIDOutput = mOutputMinimumValue;
+        }
+
+        if (mIntegralAccumulator > mOutputMaximumValue) {
+            mIntegralAccumulator = mOutputMaximumValue;
+        } else if (mIntegralAccumulator < mOutputMinimumValue) {
+            mIntegralAccumulator = mOutputMinimumValue;
+        }
+    }
+
+
+    /**
+     * SetMode : Set the PID controller to PIDDLE_AUTOMATIC or PIDDLE_MANUAL
+     * in Manual mode compute() will do nothing.
+     *
+     * @param mode PIDDLE_AUTOMATIC or PIDDLE_MANUAL
+     */
+    public void setMode(int mode) {
+        boolean newAuto = (mode == PIDDLE_AUTOMATIC);
+
+        if (newAuto == !mAutomaticMode) {
+            initialize();
+        }
+        mAutomaticMode = newAuto;
+    }
+
+
+    /**
+     * initialize : This is called by SetMode when Automatic is enabled to resume PID operations without
+     * hopefully any 'jumps' or 'kicks'.  It is not intended to be called from outside the class
+     * (thus the private designation)
+     */
+    private void initialize() {
+        mLastInputValue = mCurrentInput;
+        mIntegralAccumulator = mPIDOutput;
+
+        if (mIntegralAccumulator > mOutputMaximumValue) {
+            mIntegralAccumulator = mOutputMaximumValue;
+        } else if (mIntegralAccumulator < mOutputMinimumValue) {
+            mIntegralAccumulator = mOutputMinimumValue;
+        }
+    }
+
+
+    /**
+     * setControllerDirection : If the PID output should be positive PIDDLE_DIRECT should be set
+     * if the PID output is meant to be negative, PIDDLE_REVERSE should be set.
+     * This parameter auto-sets the tuning parameters to negative, NEVER send negative tuning values to
+     * kI,kP,kD
+     */
+    public void setControllerDirection(int direction) {
+        controllerDirection = direction;
+    }
+}
