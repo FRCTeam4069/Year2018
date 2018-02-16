3d4a6a5ee6c2d0c2ae17eec99781f6672ce158a1
brendon-ai <brendon-m@outlook.com>
2018-02-16 15:14:33 -0500

    cleaned up stuff

--- src/main/java/frc/team4069/robot/commands/autonomous/DriveStraightForDistanceCommand.java
+++ src/main/java/frc/team4069/robot/commands/autonomous/DriveStraightForDistanceCommand.java
@@ -7,7 +7,6 @@ public class DriveStraightForDistanceCommand extends CommandBase {
     private final double speed = 0.2;
 
     private double distanceMeters;
-    private double endDistance;
     private double signedSpeed;
 
     public DriveStraightForDistanceCommand(double distanceMeters) {
@@ -19,17 +18,14 @@ public class DriveStraightForDistanceCommand extends CommandBase {
     @Override
     protected void initialize() {
         super.initialize();
-        endDistance = driveBase.getDistanceTraveledMeters() + distanceMeters;
+        driveBase.reset();
         driveBase.driveContinuousSpeed(0, signedSpeed, true);
     }
 
     @Override
     protected boolean isFinished() {
         double distance = Math.abs(driveBase.getDistanceTraveledMeters());
-        System.out.println("Distance is " + distance);
-        System.out.println("End distance is " + endDistance);
-//        return driveBase.getDistanceTraveledMeters() >= endDistance;
-        return distance >= endDistance;
+        return distance >= distanceMeters;
     }
 
     @Override
