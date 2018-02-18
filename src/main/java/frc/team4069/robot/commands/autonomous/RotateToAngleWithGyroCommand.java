63e90d1f9521501585b253577604aa749b3f5a8f
Troy Laakso <tlaakso@eastlink.ca>
2018-02-18 14:35:32 -0500

    Moved initialization stuff to constructor in turning command

--- src/main/java/frc/team4069/robot/commands/autonomous/RotateToAngleWithGyroCommand.java
+++ src/main/java/frc/team4069/robot/commands/autonomous/RotateToAngleWithGyroCommand.java
@@ -47,6 +47,11 @@ public class RotateToAngleWithGyroCommand extends CommandBase {
         } else if (this.relativeAngle < -180) {
             this.relativeAngle += 360;
         }
+		startAngle = getGyroAngle();
+        // If passed angle to turn is positive, turn right
+        turnRight = relativeAngle > 0;
+        prevGyroscope = currentGyroscope = getGyroAngle();
+        prevTime = currentTime = startTime = System.currentTimeMillis();
     }
 
     /**
@@ -73,11 +78,7 @@ public class RotateToAngleWithGyroCommand extends CommandBase {
     }
 
     protected void initialize() {
-        startAngle = getGyroAngle();
-        // If passed angle to turn is positive, turn right
-        turnRight = relativeAngle > 0;
-        prevGyroscope = currentGyroscope = getGyroAngle();
-        prevTime = currentTime = startTime = System.currentTimeMillis();
+		
     }
 
     @Override
