721757cef27cb629dfb0c5f242a0fe59118b5615
brendon-ai <brendon-m@outlook.com>
2018-02-18 14:07:25 -0500

    Grab cube testing

--- src/main/java/frc/team4069/robot/commands/autonomous/GrabCubeCommand.java
+++ src/main/java/frc/team4069/robot/commands/autonomous/GrabCubeCommand.java
@@ -7,9 +7,9 @@ import frc.team4069.robot.commands.vacuum.StartVacuumCommand;
 import frc.team4069.robot.subsystems.ElevatorSubsystem.Position;
 
 // Series of actions to unlatch the vacuum, pick up the cube, and bring it to switch height
-public class GrabCubeCommand extends CommandGroup {
+class GrabCubeCommand extends CommandGroup {
 
-    public GrabCubeCommand() {
+    GrabCubeCommand() {
         addSequential(new SetElevatorPositionCommand(Position.SWITCH));
         addSequential(new WaitCommand(2));
         addSequential(new StartVacuumCommand());
