7b4fd1798085254ecb966d5abf10e13c63187b96
brendon-ai <brendon-m@outlook.com>
2018-02-19 17:06:42 -0500

    print position in gyro thread

--- src/main/java/frc/team4069/robot/commands/autonomous/AutonomousCommandGroup.java
+++ src/main/java/frc/team4069/robot/commands/autonomous/AutonomousCommandGroup.java
@@ -2,9 +2,6 @@ package frc.team4069.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import frc.team4069.robot.commands.elevator.ZeroElevatorCommand;
-import frc.team4069.robot.commands.vacuum.StartVacuumCommand;
-import frc.team4069.robot.commands.vacuum.StopVacuumCommand;
 
 // Command group that does everything involved in autonomous mode
 public class AutonomousCommandGroup extends CommandGroup {
@@ -18,7 +15,7 @@ public class AutonomousCommandGroup extends CommandGroup {
 
     // Constructor that runs all necessary commands in parallel
     public AutonomousCommandGroup() {
-		addSequential(new DriveStraightForDistanceCommand(6));
+        addSequential(new DriveStraightForDistanceCommand(6, 0.2));
         /*addSequential(new StartVacuumCommand());
         addSequential(new ZeroElevatorCommand());
         addSequential(new GrabCubeCommandGroup());
